08159e9fb7b2cab33beb3a8e4012121255c5d71a
justinfineberg <justinf1818@gmail.com>
2021-10-19 18:37:54 -0600

    updated endpoints

--- api/parties/party-model.js
+++ api/parties/party-model.js
@@ -1,61 +1,81 @@
-const db = require('../data/db-config')
-const { generateRandomString } = require('./party-middleware')
+const db = require("../data/db-config");
+const { generateRandomString } = require("./party-middleware");
 
-async function getAllPartiesByUser(user_id){
-   const allParties = await db("parties").where('user_id', user_id)
-   return allParties
+async function getAllPartiesByUser(user_id) {
+  const allParties = await db("parties").where("user_id", user_id);
+  return allParties;
 }
 
-async function getAllItemsForParty(party_id){
-    const items = await db("parties_items").where('party_id', party_id)
-    return items
+async function getAllItemsForParty(party_id) {
+  const items = await db("parties_items").where("party_id", party_id);
+  return items;
 }
 
-async function addItem(item, party_id){
-    const [newItemObject] = await db("parties_items").insert({
-        completed: item.completed,
-        person_bringing: item.person_bringing,
-        party_id: party_id,
-        item_name: item.item_name
-    }, [
-        "completed",
-        "person_bringing",
-        "party_id",
-        "item_name"
-    ])
-    return newItemObject
-}
-
-async function addParty(user_id, party){
-
-    const [newPartyObject] = await db("parties").insert({
-        date: party.date,
-        time: party.time,
-        location: party.location,
-        party_url_string: generateRandomString(5),
-        user_id: user_id
+async function addItem(item, party_id) {
+  const [newItemObject] = await db("parties_items").insert(
+    {
+      completed: item.completed,
+      person_bringing: item.person_bringing,
+      party_id: party_id,
+      item_name: item.item_name,
     },
-    [
-        "party_id",
-        "date",
-        "time",
-        "location",
-        "party_url_string",
-        "user_id"
+    ["completed", "person_bringing", "party_id", "item_name"]
+  );
+  return newItemObject;
+}
+
+async function addParty(user_id, party) {
+  const [newPartyObject] = await db("parties").insert(
+    {
+      date: party.date,
+      time: party.time,
+      location: party.location,
+      party_url_string: generateRandomString(5),
+      user_id: user_id,
+    },
+    ["party_id", "date", "time", "location", "party_url_string", "user_id"]
+  );
+  return newPartyObject;
+}
+
+async function removeParty(party_id) {
+  const deleted = db("parties").where("party_id", party_id).del();
+  return deleted;
+}
+
+async function updateItem(party_item_id, changes) {
+  const updated = db("parties_items")
+    .update(changes, [
+      "party_item_id",
+      "completed",
+      "person_bringing",
+      "item_name",
+      "party_id",
     ])
-    return newPartyObject
+    .where("party_item_id", party_item_id);
+  return updated;
 }
 
-async function removeParty(party_id){
-    const deleted = db('parties').where("party_id", party_id).del()
-    return deleted
+async function updateParty(party_id, changes) {
+  const updated = db("parties")
+    .update(changes, [
+      "party_id",
+      "date",
+      "time",
+      "location",
+      "party_url_string",
+      "user_id",
+    ])
+    .where("party_id", party_id);
+  return updated;
 }
-
 
 module.exports = {
-    getAllPartiesByUser,
-    getAllItemsForParty,
-    addItem,
-    addParty,
-    removeParty
-}
+  getAllPartiesByUser,
+  getAllItemsForParty,
+  addItem,
+  addParty,
+  removeParty,
+  updateItem,
+  updateParty,
+};
--- api/parties/party-router.js
+++ api/parties/party-router.js
@@ -1,53 +1,87 @@
 const express = require("express");
 const router = express.Router();
-const Parties = require('./party-model')
-const { restricted } = require("../users/user-middleware")
+const Parties = require("./party-model");
+const { restricted } = require("../users/user-middleware");
 
-router.get("/parties", restricted, async(req, res, next)=>{
-    try {
-        const allParties = await Parties.getAllPartiesByUser(req.decodedToken.user_id)
-        res.json(allParties)
-    }catch (err){
-        res.json(err)
-    }
-})
+//This will get you all the parties for a user.
+router.get("/parties", restricted, async (req, res, next) => {
+  try {
+    const allParties = await Parties.getAllPartiesByUser(
+      req.decodedToken.user_id
+    );
+    res.json(allParties);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
-router.post("/parties", restricted, async(req, res, next)=>{
-    try {
-        const { user_id } = req.decodedToken
-        const newParty = await Parties.addParty(user_id, req.body)
-        res.json(newParty)
-    } catch (err){
-        res.json(err)
-    }
-})
+//This will add a new party for a user
+router.post("/parties", restricted, async (req, res, next) => {
+  try {
+    const { user_id } = req.decodedToken;
+    const newParty = await Parties.addParty(user_id, req.body);
+    res.json(newParty);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
-router.delete('/parties/:party_id', restricted, async(req, res, next)=>{
-    try {
-        const deleted = Parties.removeParty(req.params.party_id)
-        res.json(deleted)
-    } catch (err){
-        next(err)
-    }
-})
+//This will update a specific party
+router.put("/parties/update/:party_id", restricted, async (req, res, next) => {
+  try {
+    const updatedParty = await Parties.updateParty(
+      req.params.party_id,
+      req.body
+    );
+    res.json(updatedParty);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get("/items/:party_id", async(req,res,next)=>{
-    try{
-        const allItemsForParty = await Parties.getAllItemsForParty(req.params.party_id)
-        res.json(allItemsForParty)
-    } catch (err){
-        next(err)
-    }
-})
+//This will delete a party
+router.delete("/parties/:party_id", restricted, async (req, res, next) => {
+  try {
+    const deleted = Parties.removeParty(req.params.party_id);
+    res.json(deleted);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post("/items/:party_id", restricted, async(req, res, next)=>{
-    try {
-        const newItem = await Parties.addItem(req.body, req.params.party_id)
-        res.json(newItem)
-    }catch (err){
-        next(err)
-    }
-})
+//This will get all the items for a specific party.
+router.get("/items/:party_id", async (req, res, next) => {
+  try {
+    const allItemsForParty = await Parties.getAllItemsForParty(
+      req.params.party_id
+    );
+    res.json(allItemsForParty);
+  } catch (err) {
+    next(err);
+  }
+});
 
+//This will update a specific item for the party.
+router.put("/items/update/:party_item_id", async (req, res, next) => {
+  try {
+    const updatedItem = await Parties.updateItem(
+      req.params.party_item_id,
+      req.body
+    );
+    res.json(updatedItem);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+//This will add a new item to the party if user logged in. Only hosts can add items.
+router.post("/items/:party_id", restricted, async (req, res, next) => {
+  try {
+    const newItem = await Parties.addItem(req.body, req.params.party_id);
+    res.json(newItem);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
